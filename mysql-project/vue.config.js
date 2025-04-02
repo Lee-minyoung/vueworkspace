caba84dc059e76c5172a07b8e74d93961b2dd7af
Lee min young <lmy1147@naver.com>
2025-04-02 17:47:22 +0900

    0402

---
+++ mysql-project/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+const serverOrigin = 'http://localhost:3000';
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer : {
+    proxy : {
+      '^/api' : {
+        target : serverOrigin, // 변경할 origin
+        changeOrigin : true,
+        ws : false, // 웹소켓 설정
+        pathRewrite : { '^/api' : '/'} // 경로재작성
+      }
+    }
+  }
+})
